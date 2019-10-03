7b9949b18fa836a1ed61e69d0a31e96560952ebd
Israel Saeta Pérez <israel@travelperk.com>
2019-10-03 18:29:22 +0200

    Use ViewSets and Routers

--- tutorial/recipes/urls.py
+++ tutorial/recipes/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from tutorial.recipes import views
 
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'recipes', views.RecipeViewSet)
+
+# The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('recipes/', views.RecipeList.as_view()),
-    path('recipes/<int:pk>/', views.RecipeDetail.as_view()),
+    path('', include(router.urls)),
 ]
