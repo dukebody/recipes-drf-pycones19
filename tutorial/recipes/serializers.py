228e378409a675bc77cefa68e233117b9422c8a4
Israel Saeta Pérez <israel@travelperk.com>
2019-10-03 18:43:23 +0200

    Add ingredients to recipe representation

--- tutorial/recipes/serializers.py
+++ tutorial/recipes/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from tutorial.recipes.models import Recipe
+from tutorial.recipes.models import Recipe, Ingredient
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['name']
 
 
 class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = IngredientSerializer(many=True, source='ingredient_set', required=False)
+
     class Meta:
         model = Recipe
-        fields = ['id', 'name', 'description']
+        fields = ['id', 'name', 'description', 'ingredients']
