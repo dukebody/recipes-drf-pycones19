830d7a3f406b0d313e155ce065b359455e150104
Israel Saeta Pérez <israel@travelperk.com>
2019-10-03 18:07:31 +0200

    Transform to class-based DRF views

--- tutorial/recipes/views.py
+++ tutorial/recipes/views.py
@@ -1,48 +1,49 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
 from tutorial.recipes.models import Recipe
 from tutorial.recipes.serializers import RecipeSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
-@csrf_exempt
-def recipes_detail(request, pk):
-    try:
-        recipe = Recipe.objects.get(pk=pk)
-    except Recipe.DoesNotExist:
-        return HttpResponse(status=404)
+class RecipeDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Recipe.objects.get(pk=pk)
+        except Recipe.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk):
+        recipe = self.get_object(pk)
         serializer = RecipeSerializer(recipe)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = RecipeSerializer(recipe, data=data)
+    def put(self, request, pk):
+        recipe = self.get_object(pk)
+        serializer = RecipeSerializer(recipe, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk):
+        recipe = self.get_object(pk)
         recipe.delete()
-        return HttpResponse(status=204)
+        return Response(status=tatus.HTTP_204_NO_CONTENT)
 
 
-@csrf_exempt  # bypass csrf checks on POST
-def recipe_list(request):
+class RecipeList(APIView):
     """
     List all recipes or create new ones.
     """
-    if request.method == 'GET':
+    def get(self, request):
         recipes = Recipe.objects.all()
         serializer = RecipeSerializer(recipes, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = RecipeSerializer(data=data)
+    def post(self, request):
+        serializer = RecipeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
