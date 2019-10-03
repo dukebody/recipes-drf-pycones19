a8cacd57ffd47ca08be667d3972a532e197c3697
Israel Saeta Pérez <israel@travelperk.com>
2019-10-03 17:47:33 +0200

    Allow creating new recipes

--- tutorial/recipes/views.py
+++ tutorial/recipes/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from tutorial.recipes.models import Recipe
 from tutorial.recipes.serializers import RecipeSerializer
@@ -14,10 +15,20 @@ def recipes_detail(request, pk):
     return JsonResponse(serializer.data)
 
 
+@csrf_exempt  # bypass csrf checks on POST
 def recipe_list(request):
     """
-    List all recipes.
+    List all recipes or create new ones.
     """
-    recipes = Recipe.objects.all()
-    serializer = RecipeSerializer(recipes, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    if request.method == 'GET':
+        recipes = Recipe.objects.all()
+        serializer = RecipeSerializer(recipes, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = RecipeSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
