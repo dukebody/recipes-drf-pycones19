33c26a1f505a9eda83f8338a2eb75f5225e2b8c6
Israel Saeta Pérez <israel@travelperk.com>
2019-10-03 18:23:03 +0200

    Use DRF Generic Views

--- tutorial/recipes/views.py
+++ tutorial/recipes/views.py
@@ -1,49 +1,17 @@
 from tutorial.recipes.models import Recipe
 from tutorial.recipes.serializers import RecipeSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 
 
-class RecipeDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Recipe.objects.get(pk=pk)
-        except Recipe.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        recipe = self.get_object(pk)
-        serializer = RecipeSerializer(recipe)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        recipe = self.get_object(pk)
-        serializer = RecipeSerializer(recipe, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        recipe = self.get_object(pk)
-        recipe.delete()
-        return Response(status=tatus.HTTP_204_NO_CONTENT)
+class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
 
 
-class RecipeList(APIView):
+class RecipeList(generics.ListCreateAPIView):
     """
     List all recipes or create new ones.
     """
-    def get(self, request):
-        recipes = Recipe.objects.all()
-        serializer = RecipeSerializer(recipes, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = RecipeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    
